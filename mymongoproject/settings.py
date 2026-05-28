5195b224256197a5362b1df021f1f3a9f275e499
chintubhat08 <chintubhat52@example.com>
2026-05-28 18:39:10 +0530

    fixed database config

--- mymongoproject/settings.py
+++ mymongoproject/settings.py
@@ -71,11 +71,8 @@ WSGI_APPLICATION = 'mymongoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'swaplearn',
-        'CLIENT': {
-            'host': config('MONGO_URL')
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
